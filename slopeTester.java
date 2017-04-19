2fe628915c81641438e2e96efdeaaf91bce859f1
Joanne Wardell <jawardell@valdosta.edu>
2017-04-19 01:00:43 -0400

    fixed g = 1 problem

--- slopeTester.java
+++ slopeTester.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class slopeTester {
 	public static void main(String[] args) {
 		int n = 0, g = 0, a = 0, m = 0;
@@ -38,69 +39,94 @@ public class slopeTester {
 				array = makeArray(n, g, a, m);
 				break;
 		}
-		for(int row = 0; row < array.length; row++) {
-			for(int col = 0; col < array[0].length; col++) {
-				if(col == array[0].length-1) {
-					if(array[row][col] == -1) {
+		for (int row = 0; row < array.length; row++) {
+			for (int col = 0; col < array[0].length; col++) {
+				if (col == array[0].length - 1) {
+					if (array[row][col] == -1) {
 						System.out.printf("\u001b[1;31m%13s\n\u001b[0m", "#");
 					} else {
-						System.out.printf("%13d\n",array[row][col]);
+						System.out.printf("%13d\n", array[row][col]);
 					}
 				} else {
-					if(array[row][col] == -1) {
+					if (array[row][col] == -1) {
 						System.out.printf("\u001b[1;31m%13s\u001b[0m", "#");
 					} else {
-						System.out.printf("%13d",array[row][col]);
+						System.out.printf("%13d", array[row][col]);
 					}
 				}
 			}
 		}
 	}
+
 	public static int[][] makeArray(int n, int g) {
-		int[][] array = new int[g + ((n+1)/2)][n];
-		array[array.length-1][0] = 1;
-		array[(array.length-g)-1][0] = -1;
-		int rowneg = (array.length-g)-2;
-		for(int col = 1; col <  array[0].length; col++) {
-			if(rowneg == -1) { break; }
-			array[rowneg][col] = -1;
-			if(col % 2 == 0) { rowneg -= 1; }
+		int[][] array = new int[g + ((n + 1) / 2)][n];
+		array[array.length - 1][0] = 1;
+		array[(array.length - g) - 1][0] = -1;
+		int rowneg = (array.length - g) - 2;
+
+		if (g == 1) {
+			array[array.length - 1][2] = 1;
 		}
-		for(int col = 1; col <= array[0].length-1; col++) {
-			for(int row = array.length-1; (array[row][col] != -1); row--) {
-				if((row == 0)) { array[row][col] = 0; break;}
-				if(row == array.length-1) {
-					array[row][col] = array[row-1][col-1];
+
+		for (int col = 1; col < array[0].length; col++) {
+			if (rowneg == -1) {
+				break;
+			}
+			array[rowneg][col] = -1;
+			if (col % 2 == 0) {
+				rowneg -= 1;
+			}
+		}
+
+		for (int col = 1; col <= array[0].length - 1; col++) {
+			for (int row = array.length - 1; (array[row][col] != -1); row--) {
+				boolean onecase = (g == 1) && ((row == array.length - 1) && (col == 1));
+				if ((row == 0)) {
+					array[row][col] = 0;
+					break;
 				}
-				else if(array[row-1][col-1] == -1) {
-					array[row][col] = array[row+1][col-1];
+				if (row == array.length - 1 && !onecase) {
+					array[row][col] = array[row - 1][col - 1];
+				} else if ((array[row - 1][col - 1] == -1)) {
+					if (row != array.length - 1) {
+						array[row][col] = array[row + 1][col - 1];
+						array[row][col] = array[row + 1][col - 1];
+					}
+
 				} else {
-					array[row][col] = array[row-1][col-1] + array[row+1][col-1];
+					array[row][col] = array[row - 1][col - 1] + array[row + 1][col - 1];
 				}
 			}
 		}
 		return array;
 	}
+
 	public static int[][] makeArray(int n, int g, int a) {
-		int[][] array = new int[g + ((n+1)/2)][n];
-		array[array.length-a][0] = 1;
-		array[(array.length-g)-1][0] = -1;
-		int rowneg = (array.length-g)-2;
-		for(int col = 1; col < array [0].length; col++) {
-			if(rowneg == -1) {break;}
+		int[][] array = new int[g + ((n + 1) / 2)][n];
+		array[array.length - a][0] = 1;
+		array[(array.length - g) - 1][0] = -1;
+		int rowneg = (array.length - g) - 2;
+		for (int col = 1; col < array[0].length; col++) {
+			if (rowneg == -1) {
+				break;
+			}
 			array[rowneg][col] = -1;
-			if(col % 2 == 0) {rowneg -= 1;}
+			if (col % 2 == 0) {
+				rowneg -= 1;
+			}
 		}
-		for(int col = 1; col <= array[0].length-1; col++) {
-			for(int row = array.length-1; (array[row][col] != -1); row--) {
-				if((row == 0)) { array[row][col] = 0; break;}
-				if(row == array.length-1) {
-					array[row][col] = array[row-1][col-1];
+		for (int col = 1; col <= array[0].length - 1; col++) {
+			for (int row = array.length - 1; (array[row][col] != -1); row--) {
+				if ((row == 0)) {
+					array[row][col] = 0;
+					break;
 				}
-				else if(array[row-1][col-1] == -1) {
-					array[row][col] = array[row+1][col-1];
+				if (row == array.length - 1) {
+					array[row][col] = array[row - 1][col - 1];
+				} else if (array[row - 1][col - 1] == -1) {
+					array[row][col] = array[row + 1][col - 1];
 				} else {
-					array[row][col] = array[row-1][col-1] + array[row+1][col-1];
+					array[row][col] = array[row - 1][col - 1] + array[row + 1][col - 1];
 				}
 			}
 		}
@@ -108,25 +134,31 @@ public class slopeTester {
 	}
 
 	public static int[][] makeArray(int n, int g, int a, int m) {
-		int[][] array = new int[g + ((n+1)/2)][n];
-		array[array.length-a][0] = 1;
-		array[(array.length-g)-1][0] = -1;
-		int rowneg = (array.length-g)-2;
-		for(int col = 1; col <  array[0].length; col++) {
-			if(rowneg == -1) { break; }
+		int[][] array = new int[g + ((n + 1) / 2)][n];
+		array[array.length - a][0] = 1;
+		array[(array.length - g) - 1][0] = -1;
+		int rowneg = (array.length - g) - 2;
+		for (int col = 1; col < array[0].length; col++) {
+			if (rowneg == -1) {
+				break;
+			}
 			array[rowneg][col] = -1;
-			if(col % m == 0) { rowneg -= 1; }
+			if (col % m == 0) {
+				rowneg -= 1;
+			}
 		}
-		for(int col = 1; col <= array[0].length-1; col++) {
-			for(int row = array.length-1; (array[row][col] != -1); row--) {
-				if((row == 0)) { array[row][col] = 0; break;}
-				if(row == array.length-1) {
-					array[row][col] = array[row-1][col-1];
+		for (int col = 1; col <= array[0].length - 1; col++) {
+			for (int row = array.length - 1; (array[row][col] != -1); row--) {
+				if ((row == 0)) {
+					array[row][col] = 0;
+					break;
 				}
-				else if(array[row-1][col-1] == -1) {
-					array[row][col] = array[row+1][col-1];
+				if (row == array.length - 1) {
+					array[row][col] = array[row - 1][col - 1];
+				} else if (array[row - 1][col - 1] == -1) {
+					array[row][col] = array[row + 1][col - 1];
 				} else {
-					array[row][col] = array[row-1][col-1] + array[row+1][col-1];
+					array[row][col] = array[row - 1][col - 1] + array[row + 1][col - 1];
 				}
 			}
 		}
