d202821e75cc2199f8f515fe8d8091e9f2e064bd
Joanne Wardell <jawardell@valdosta.edu>
2017-07-26 14:13:20 -0400

    putting here until make separate repo

--- Driver.java
+++ Driver.java
@@ -8,7 +8,11 @@ public class Driver {
 
 
 	/* 
-	Return an array that contains exactly the same numbers as the given 	array,but rearranged so that every 3 is immediately followed by a 4. Do not 	move the 3's, but every other number may move. The array contains the same 	number of 3's and 4's, every 3 has a number after it that is not a 3, and a 	3 appears in the array before any 4.
+	Return an array that contains exactly the same numbers as the given
+ 	array,but rearranged so that every 3 is immediately followed by a 4. Do not
+ 	move the 3's, but every other number may move. The array contains the same
+ 	number of 3's and 4's, every 3 has a number after it that is not a 3, and a
+ 	3 appears in the array before any 4.
 	*/
 	public static int[] fix34(int[] nums) {
 		int index = 0;
@@ -33,7 +37,12 @@ public class Driver {
 
 	
 	/* 
-	(This is a slightly harder version of the fix34 problem.) Return an array 		that contains exactly the same numbers as the given array, but rearranged 		so that every 4 is immediately followed by a 5. Do not move the 4's, but 		every other number may move. The array contains the same number of 4's and 		5's, and every 4 has a number after it that is not a 4. In this version, 		5's may appear anywhere in the original array.
+	(This is a slightly harder version of the fix34 problem.) Return an array
+ 	that contains exactly the same numbers as the given array, but rearranged
+	so that every 4 is immediately followed by a 5. Do not move the 4's, but
+ 	every other number may move. The array contains the same number of 4's and
+ 	5's, and every 4 has a number after it that is not a 4. In this version, 	
+	5's may appear anywhere in the original array.
 	*/
 	public static int[] fix45(int[] nums) {
 		for(int i = 0; i < nums.length-1; i++) {
@@ -63,7 +72,9 @@ public class Driver {
 	
 
 	/*
-	 Given a non-empty array, return true if there is a place to split the 	array so that the sum of the numbers on one side is equal to the sum of the 	numbers on the other side.
+	Given a non-empty array, return true if there is a place to split the
+ 	array so that the sum of the numbers on one side is equal to the sum of the
+ 	numbers on the other side.
 	*/
 	public static boolean canBalance(int[] nums) {
 		int sum1 = 0, sum2 = 0, pos = 0;
@@ -87,7 +98,10 @@ public class Driver {
 
 
 	/*
-	Given two arrays of ints sorted in increasing order, outer and inner, 	return true if all of the numbers in inner appear in outer. The best 	solution makes only a single "linear" pass of both arrays, taking advantage 	of the fact that both arrays are already in sorted order.	 
+	Given two arrays of ints sorted in increasing order, outer and inner,
+ 	return true if all of the numbers in inner appear in outer. The best
+ 	solution makes only a single "linear" pass of both arrays, taking advantage
+ 	of the fact that both arrays are already in sorted order.	 
 	*/
 	public static boolean linearIn(int[] outer, int[] inner) {
 		int pos = 0;
