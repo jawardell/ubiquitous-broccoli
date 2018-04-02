3e087f7bd952ea44120f76b6870ec5887ff7e371
Joanne Wardell <jawardell@valdosta.edu>
2018-04-02 01:53:41 -0400

    added pstricks yay

--- array.js
+++ array.js
@@ -1,62 +1,76 @@
-	var count = 0;
-	function dummy() {
-		document.getElementById("here").innerHTML = "yep<br />ok";
-		var array = [];
-		var string = "";
-		for(var i = 0; i < 4; i++) array[i] = [];
-		for(var i = 0; i < array.length; i++) {
-			for(var j = 0; j < 4; j++) {
-				array[i][j] = 5.0;
-				string += " " + array[i][j].toString() + " ";
-				if(j == 3) {
-					string += "<br />";
-				}
-			}
-		}
-			
-		document.getElementById("here").innerHTML = string;
-	}
-
-
-	function makeArray(n, g, r) {
-	  	//gather arguments from webpage
-		n = parseFloat(document.getElementById("n").value);
-		g = parseFloat(document.getElementById("g").value);
-		r = parseFloat(document.getElementById("r").value);
-	  	var string = "";
-		if(isNaN(n) || isNaN(g)) {
-			count++;
-			document.getElementById("here").innerHTML = "stop that!\n" + count.toString();
-			return;
-		}
-
-
-		//build and initialize array default values
-		var array = [];
-		var numrows = (n % 2 == 0) ? (n+2*g+1) : (n+2*g+2);
-		for(var i = 0; i < numrows; i++) array[i] = [];	
-		for(var i = 0; i < array.length; i++) {
-			for(var j = 0; j < n+1; j++) {
-				array[i][j] = 0;
-			}
-		}
-		array[Math.floor(numrows/2)+1][0] = -1;
-		array[Math.floor(numrows/2)-1][0] = 1;
+var n,
+	g, 
+	r,
+	count;
 
 
 
-
-
-
-		//prepare output string and put on page
-		for(var i = 0; i < array.length; i++) {
-			for(var j = 0; j < n+1; j++) {	
-				string += "\t" + array[i][j].toString() + "\t";
-				if(j == n) {
-					string += "\n";
-				}
-			}
-		}
-		document.getElementById("here").innerHTML = string;
+function makeArray() {
+  	//gather arguments from webpage
+	count = 0;
+  	var string = "";
 	
+
+	//build and initialize array default values
+	var array = [];
+	var numrows = (n % 2 == 0) ? (n+2*g+1) : (n+2*g+2);
+
+	//initialize array to all 0.0 entries
+	array = initarray(numrows, n+1);
+
+	//perform skewed corridor calculations on array
+	//and update current array to newly calculated array
+	array = calcskewed(array);
+
+	document.getElementById("here").innerHTML = array2str(array);
+	
+}
+
+
+function calcskewed(array) {	
+	array[Math.floor(array.length/2)+1][0] = -1;
+	array[Math.floor(array.length/2)-1][0] = 1;
+	return array;
+}
+
+
+function parseinput(){
+	if(isNaN(n) || isNaN(g)) {
+		count++;
+		document.getElementById("here").innerHTML = "stop that!\n" + count.toString();
+		return false;
 	}
+
+	n = parseFloat(document.getElementById("n").value);
+	g = parseFloat(document.getElementById("g").value);
+	r = parseFloat(document.getElementById("r").value);
+	return true;
+}
+
+
+
+function initarray(rows, cols) {
+	var array = [];
+	for(var i = 0; i < rows; i++) array[i] = [];
+	for(var i = 0; i < rows; i++) {
+		for(var j = 0; j < cols; j++) {
+			arary[i][j] = 0.0;
+		}
+	}
+	return array;
+}
+
+
+function array2str(array) {
+	var res = "";
+	for(var i = 0; i < array.length; i++) {
+		for(var j = 0; j < array[0].length; j++) {
+			if(j == array[0].length - 1) {
+				res += array[i][j].toString() + "\n";
+			} else {
+				res += arary[i][j].toString() + " ";
+			}
+		}
+	}
+	return res;
+}
